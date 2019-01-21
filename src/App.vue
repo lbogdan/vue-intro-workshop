96f9ac25d1a5ab10d402a4db1de04f1603ee167c
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-21 19:44:51 +0200

    Send movie data to MovieItem as a prop.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <movie-item />
+    <movie-item :item="movie" />
   </div>
 </template>
 
@@ -11,6 +11,20 @@ export default {
   name: 'app',
   components: {
     MovieItem,
+  },
+  data() {
+    return {
+      movie: {
+        title: 'Star Wars: The Last Jedi',
+        year: 2017,
+        genre: 'Action, Adventure, Fantasy',
+        plot:
+          'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+        poster:
+          'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
+        comment: '',
+      },
+    };
   },
 };
 </script>
--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -41,19 +41,11 @@
 <script>
 export default {
   name: 'movie-item',
-  data() {
-    return {
-      item: {
-        title: 'Star Wars: The Last Jedi',
-        year: 2017,
-        genre: 'Action, Adventure, Fantasy',
-        plot:
-          'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
-        poster:
-          'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
-        comment: '',
-      },
-    };
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
   },
   computed: {
     commentWordCount() {
