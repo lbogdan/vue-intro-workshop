2bf9db449bdb7c379526b6b03a1b037b7436ebcc
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-21 16:10:52 +0200

    Extract movie content to component state.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
-    <h1>Star Wars: The Last Jedi (2017)</h1>
-    <h2>Action, Adventure, Fantasy</h2>
+    <h1>{{ title }} ({{ year }})</h1>
+    <h2>{{ genre }}</h2>
     <p>
-      Rey develops her newly discovered abilities with the guidance of Luke
-      Skywalker, who is unsettled by the strength of her powers. Meanwhile, the
-      Resistance prepares to do battle with the First Order.
+      {{ plot }}
     </p>
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg"
-      alt="poster"
-    />
+    <img v-bind:src="poster" alt="poster" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      title: 'Star Wars: The Last Jedi',
+      year: 2017,
+      genre: 'Action, Adventure, Fantasy',
+      plot:
+        'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+      poster:
+        'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
+    };
+  },
 };
 </script>
