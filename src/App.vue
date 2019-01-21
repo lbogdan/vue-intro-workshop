8a0c1a508f8706b2ceb046b6eb57234272c92889
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-21 16:10:10 +0200

    Add movie content to App.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <h1>Welcome to Your Vue.js App</h1>
+    <h1>Star Wars: The Last Jedi (2017)</h1>
+    <h2>Action, Adventure, Fantasy</h2>
+    <p>
+      Rey develops her newly discovered abilities with the guidance of Luke
+      Skywalker, who is unsettled by the strength of her powers. Meanwhile, the
+      Resistance prepares to do battle with the First Order.
+    </p>
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg"
+      alt="poster"
+    />
   </div>
 </template>
 
