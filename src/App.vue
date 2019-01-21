6a7757fff67e3e3ba16015c0a9dd06a30536a273
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-21 16:29:52 +0200

    Extract movie template and state to a MovieItem component.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app">
-    <h1>{{ title }} ({{ year }})</h1>
-    <h2>{{ genre }}</h2>
-    <p>
-      {{ plot }}
-    </p>
-    <img v-bind:src="poster" alt="poster" />
+    <movie-item />
   </div>
 </template>
 
 <script>
+import MovieItem from './components/MovieItem.vue';
+
 export default {
   name: 'app',
-  data() {
-    return {
-      title: 'Star Wars: The Last Jedi',
-      year: 2017,
-      genre: 'Action, Adventure, Fantasy',
-      plot:
-        'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
-      poster:
-        'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
-    };
+  components: {
+    MovieItem,
   },
 };
 </script>
---
+++ src/components/MovieItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ title }} ({{ year }})</h1>
+    <h2>{{ genre }}</h2>
+    <p>
+      {{ plot }}
+    </p>
+    <img v-bind:src="poster" alt="poster" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'movie-item',
+  data() {
+    return {
+      title: 'Star Wars: The Last Jedi',
+      year: 2017,
+      genre: 'Action, Adventure, Fantasy',
+      plot:
+        'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+      poster:
+        'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
+    };
+  },
+};
+</script>
