346bf845466a9df67a69d15c1c5d2c866df06d35
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-22 19:18:19 +0200

    Add LoadingIndicator component and simple "api".

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+const initialMovies = [
+  {
+    id: 1,
+    title: 'Star Wars: The Last Jedi',
+    year: 2017,
+    genre: 'Action, Adventure, Fantasy',
+    plot:
+      'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+    poster: 'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
+    comment: '',
+  },
+  {
+    id: 2,
+    title: 'Black Swan',
+    year: 2010,
+    genre: 'Drama, Thriller',
+    plot:
+      'A committed dancer wins the lead role in a production of Tchaikovskys "Swan Lake" only to find herself struggling to maintain her sanity.',
+    poster:
+      'https://images-na.ssl-images-amazon.com/images/I/615yWgAir2L._SY500_.jpg',
+    comment: '',
+  },
+  {
+    id: 3,
+    title: 'Fight Club',
+    year: 1999,
+    genre: 'Drama',
+    plot:
+      'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.',
+    poster: 'https://images-na.ssl-images-amazon.com/images/I/51iOANjtCQL.jpg',
+    comment: '',
+  },
+  {
+    id: 4,
+    title: 'The Godfather: Part II',
+    year: 1974,
+    genre: 'Crime, Drama',
+    plot:
+      'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
+    poster: 'https://images-na.ssl-images-amazon.com/images/I/41V2AB34KCL.jpg',
+    comment: '',
+  },
+];
+
+export function getMovies() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(initialMovies), 500 + 500 * Math.random());
+  });
+}
---
+++ src/components/LoadingIndicator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col d-flex align-items-center justify-content-center mt-5">
+      <div class="spinner-border mr-2" role="status" aria-hidden="true"></div>
+      <strong v-if="message">{{ message }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loading',
+  props: {
+    message: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
