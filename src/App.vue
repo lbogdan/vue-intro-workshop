8ce67987f41a38bb409a95a3a5669cdc3779c67e
Bogdan Luca <luca.bogdan@gmail.com>
2019-01-22 19:19:09 +0200

    Load movies from api and show loading indicator while loading.

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,35 @@
 <template>
   <div id="app" class="container mb-4">
-    <div class="row">
+    <loading-indicator v-if="isLoading" message="Loading movies..." />
+    <div v-else class="row">
       <movie-item v-for="movie in movies" :item="movie" :key="movie.id" />
     </div>
   </div>
 </template>
 
 <script>
+import { getMovies } from './api';
 import MovieItem from './components/MovieItem.vue';
+import LoadingIndicator from './components/LoadingIndicator.vue';
 
 export default {
   name: 'app',
   components: {
     MovieItem,
+    LoadingIndicator,
   },
   data() {
     return {
-      movies: [
-        {
-          id: 1,
-          title: 'Star Wars: The Last Jedi',
-          year: 2017,
-          genre: 'Action, Adventure, Fantasy',
-          plot:
-            'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
-          poster:
-            'https://images-na.ssl-images-amazon.com/images/I/51ih4cPagFL.jpg',
-          comment: '',
-        },
-        {
-          id: 2,
-          title: 'Black Swan',
-          year: 2010,
-          genre: 'Drama, Thriller',
-          plot:
-            'A committed dancer wins the lead role in a production of Tchaikovskys "Swan Lake" only to find herself struggling to maintain her sanity.',
-          poster:
-            'https://images-na.ssl-images-amazon.com/images/I/615yWgAir2L._SY500_.jpg',
-          comment: '',
-        },
-        {
-          id: 3,
-          title: 'Fight Club',
-          year: 1999,
-          genre: 'Drama',
-          plot:
-            'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.',
-          poster:
-            'https://images-na.ssl-images-amazon.com/images/I/51iOANjtCQL.jpg',
-          comment: '',
-        },
-        {
-          id: 4,
-          title: 'The Godfather: Part II',
-          year: 1974,
-          genre: 'Crime, Drama',
-          plot:
-            'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
-          poster:
-            'https://images-na.ssl-images-amazon.com/images/I/41V2AB34KCL.jpg',
-          comment: '',
-        },
-      ],
+      movies: [],
+      isLoading: false,
     };
+  },
+  created() {
+    this.isLoading = true;
+    getMovies().then(movies => {
+      this.movies = movies;
+      this.isLoading = false;
+    });
   },
 };
 </script>
