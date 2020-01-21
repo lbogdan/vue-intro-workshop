950cb73175d006caaaf69e750812f8a6e2e74c42
Bogdan Luca <luca.bogdan@gmail.com>
2020-01-21 15:32:14 +0200

    Add VUE_APP_API_PORT environment variable.

--- src/api-rest/index.js
+++ src/api-rest/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const apiPort = process.env.VUE_APP_API_PORT || 8000;
+
 const api = axios.create({
   baseURL:
     window.location.host.indexOf('codesandbox') === -1
-      ? 'http://localhost:8000/'
+      ? `http://localhost:${apiPort}/`
       : 'https://lpykmjrl9l.sse.codesandbox.io/',
 });
 
